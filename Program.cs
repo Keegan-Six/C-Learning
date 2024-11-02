aa2d5e22df2e30d8008f8ac86948e56924c0eca7
keegan <ksmith3@iastate.edu>
2024-11-02 08:13:15 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+public class Hello
+{
+    public static void Main()
+    {
+        System.Console.WriteLine("Hello!");
+        System.Console.WriteLine("Goodbye, World!");
+    }
+}
